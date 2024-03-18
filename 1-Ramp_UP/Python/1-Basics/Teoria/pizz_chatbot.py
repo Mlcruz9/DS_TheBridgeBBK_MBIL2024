55f0de1edf91e59d52a7ffec1062bb8662b387ce
miguellc <mislap87@gmail.com>
2024-03-18 09:01:46 +0100

    W2 D1

--- 1-Ramp_UP/Python/1-Basics/Teoria/pizz_chatbot.py
+++ 1-Ramp_UP/Python/1-Basics/Teoria/pizz_chatbot.py
@@ -1,8 +1,11 @@
-# pizz = input("Buenas tardes, bienvenido al servicio de pedido online, ¿Cuántas pizzas desea?: ")
-
-# direcc = input(f"Estupendo, se están preparando {pizz} pizzas!. Dígame su dirección: ")
-
-# respuesta = f"Le mandaremos {pizz} pizzas a la dirección {direcc}"
-
 def prueba():
     print('Hola mundo')
+
+if __name__ == "__main__":
+    pizz = input("Buenas tardes, bienvenido al servicio de pedido online, ¿Cuántas pizzas desea?: ")
+
+    direcc = input(f"Estupendo, se están preparando {pizz} pizzas!. Dígame su dirección: ")
+
+    respuesta = f"Le mandaremos {pizz} pizzas a la dirección {direcc}"
+
+    print(respuesta)
